355fd683c4f89e96c6f2016c1d33a87b43221166
凉笙 <103999874+Lsheng0-0@users.noreply.github.com>
2023-07-17 11:57:44 +0800

    Delete Lshengpackage directory

--- Lshengpackage/__init__.py
+++
@@ -1,8 +0,0 @@
-# !/usr/bin/env python
-# -*-coding:utf-8 -*-
-
-
-from Lshengpackage.Command_adb import command
-from Lshengpackage.Pac_adb import pic, mouse
-from Lshengpackage.Reg import RegDm
-from Lshengpackage.chaojiying import Verification
